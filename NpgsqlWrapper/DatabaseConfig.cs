19b1ebcfdae3f99324c3de2d323daaa0cd605f42
Erik Morén <erik.moren78@gmail.com>
2023-11-09 09:03:31 +0100

    Changed login data encryption

--- NpgsqlWrapper/DatabaseConfig.cs
+++ NpgsqlWrapper/DatabaseConfig.cs
@@ -4,42 +4,73 @@ namespace NpgsqlWrapper
 {
     public class DatabaseConfig
     {
-        public string Server { get; set; }
-        public int Port { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Database { get; set; }
-        public string? EncryptionKey { get; set; }
+        public string? Server { get; set; }
+        public int? Port { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Database { get; set; }
+        public byte[]? Key { get; set; }
+        public byte[]? Vector { get; set; }
+        public byte[]? Bytes { get; set; }
 
+        /// <summary>
+        /// Decrypts and mapps a DatabaseConfig object and return it to be used for login in to a database.
+        /// </summary>
+        /// <param name="configFile">File path where tha json is stored.</param>
+        /// <returns>DatabaseConfig with mapped login information.</returns>
+        /// <exception cref="ArgumentNullException">Throws if key or vector is set.</exception>
         public static DatabaseConfig Load(string configFile)
         {
             string json = File.ReadAllText(configFile);
             DatabaseConfig config = JsonSerializer.Deserialize<DatabaseConfig>(json)!;
 
-            if (config.EncryptionKey == null)
+            if (config.Key == null || config.Vector == null || config.Bytes == null)
             {
                 throw new ArgumentNullException("config key");
             }
 
-            config.Server = Encryption.DecryptString(config.EncryptionKey, config.Server);
-            config.Username = Encryption.DecryptString(config.EncryptionKey, config.Username);
-            config.Password = Encryption.DecryptString(config.EncryptionKey, config.Password);
-            config.Database = Encryption.DecryptString(config.EncryptionKey, config.Database);
+            json = Encryption.DecryptStringFromBytes(config.Bytes, config.Key, config.Vector);
 
-            return config;
+            config = JsonSerializer.Deserialize<DatabaseConfig>(json)!;
+
+            DatabaseConfig dbLogin = new();
+            dbLogin.Server = config.Server;
+            dbLogin.Port = config.Port;
+            dbLogin.Username = config.Username;
+            dbLogin.Password = config.Password;
+            dbLogin.Database = config.Database;
+
+            return dbLogin;
         }
 
+        /// <summary>
+        /// Encrypts and saves login information to a database.
+        /// </summary>
+        /// <param name="configFile">File path where tha json is stored.</param>
+        /// <param name="config">DatabaseConfig with mapped login information.</param>
+        /// <exception cref="ArgumentNullException">Throws if key or vector is set.</exception>
         public static void Save(string configFile, DatabaseConfig config)
         {
-            if (config.EncryptionKey == null)
+            if (config.Key != null && config.Vector != null)
             {
-                config.EncryptionKey = Encryption.GenerateKey();
+                throw new ArgumentNullException("config key pair");
             }
 
-            config.Server = Encryption.EncryptString(config.EncryptionKey, config.Server);
-            config.Username = Encryption.EncryptString(config.EncryptionKey, config.Username);
-            config.Password = Encryption.EncryptString(config.EncryptionKey, config.Password);
-            config.Database = Encryption.EncryptString(config.EncryptionKey, config.Database);
+            string originalJson = JsonSerializer.Serialize(config);
+            byte[] key = new byte[32];
+            byte[] vector = new byte[32];
+            Encryption.GenerateKeyPair(out key, out vector);
+
+            config.Vector = vector;
+            config.Key = key;
+
+            config.Bytes = Encryption.EncryptStringToBytes(originalJson, config.Key, config.Vector);
+
+            config.Server = null;
+            config.Port = null;
+            config.Username = null;
+            config.Password = null;
+            config.Database = null;
 
             string jsonString = JsonSerializer.Serialize(config);
             File.WriteAllText(configFile, jsonString);
--- NpgsqlWrapper/Encryption.cs
+++ NpgsqlWrapper/Encryption.cs
@@ -1,55 +1,79 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
+// Inspired of AES (Advanced Encryption Standard): https://www.c-sharpcorner.com/article/best-algorithm-for-encrypting-and-decrypting-a-string-in-c-sharp/
 namespace NpgsqlWrapper
 {
+    /// <summary>
+    /// This class encrypts and decrypts a string with AES (Advanced Encryption Standard). It encrypts data in fixed-size blocks of 128 bits, using a key size of 256 bits.
+    /// </summary>
     public class Encryption
     {
-        public static string EncryptString(string key, string plainText)
+        /// <summary>
+        /// Create an Aes object with the specified key and IV to encrypt message in plain text.
+        /// </summary>
+        /// <param name="plainText">The message to encrypt.</param>
+        /// <param name="key">Encryption secret key.</param>
+        /// <param name="iv">Encryption initialisation vector.</param>
+        /// <returns>Bytes array.</returns>
+        public static byte[] EncryptStringToBytes(string plainText, byte[] key, byte[] iv)
         {
-            byte[] iv = new byte[16];
-            byte[] array;
+            byte[] encrypted;
 
             using (Aes aes = Aes.Create())
             {
-                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.Key = key;
                 aes.IV = iv;
 
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                using MemoryStream memoryStream = new();
-                using CryptoStream cryptoStream = new(memoryStream, encryptor, CryptoStreamMode.Write);
-                using (StreamWriter streamWriter = new(cryptoStream))
+                using MemoryStream memoryStream = new MemoryStream();
+                using CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
                 {
                     streamWriter.Write(plainText);
                 }
 
-                array = memoryStream.ToArray();
+                encrypted = memoryStream.ToArray();
             }
 
-            return Convert.ToBase64String(array);
+            return encrypted;
         }
 
-        public static string DecryptString(string key, string cipherText)
+        /// <summary>
+        /// // Create an Aes object with the specified key and IV to decrypt the byte array.
+        /// </summary>
+        /// <param name="cipherText">The message to encrypt.</param>
+        /// <param name="key">Encryption secret ke.y</param>
+        /// <param name="iv">Encryption initialisation vector.</param>
+        /// <returns>String.</returns>
+        public static string DecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv)
         {
-            byte[] iv = new byte[16];
-            byte[] buffer = Convert.FromBase64String(cipherText);
+            string decrypted;
 
-            using Aes aes = Aes.Create();
-            aes.Key = Encoding.UTF8.GetBytes(key);
-            aes.IV = iv;
-            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
 
-            using MemoryStream memoryStream = new(buffer);
-            using CryptoStream cryptoStream = new(memoryStream, decryptor, CryptoStreamMode.Read);
-            using StreamReader streamReader = new(cryptoStream);
-            return streamReader.ReadToEnd();
+                using MemoryStream memoryStream = new MemoryStream(cipherText);
+                using CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+                using StreamReader streamReader = new StreamReader(cryptoStream);
+                decrypted = streamReader.ReadToEnd();
+            }
+
+            return decrypted;
         }
 
-        public static string GenerateKey()
+        /// <summary>
+        /// Generat key and initialisation vector to be used for encryption.
+        /// </summary>
+        /// <param name="key">Encryption secret key.</param>
+        /// <param name="iv">Encryption initialisation vector.</param>
+        public static void GenerateKeyPair(out byte[] key, out byte[] iv)
         {
             using Aes aes = Aes.Create();
-            return Convert.ToBase64String(aes.Key).Substring(0, 32);
+
+            key = aes.Key;
+            iv = aes.IV;
         }
     }
 }
