59d85f4e0cca5eb824432369115c20f338b141ca
Erik Morén <erik.moren78@gmail.com>
2023-11-13 16:24:04 +0100

    Updated doxygen

--- NpgsqlWrapper/doxygen/html/search/files_5.js
+++ NpgsqlWrapper/doxygen/html/search/files_5.js
@@ -1,6 +1,14 @@
 var searchData=
 [
   ['mynpgsql_2ecs_0',['MyNpgsql.cs',['../_my_npgsql_8cs.html',1,'']]],
-  ['mynpgsqlasync_2ecs_1',['MyNpgsqlAsync.cs',['../_my_npgsql_async_8cs.html',1,'']]],
-  ['mynpgsqlbase_2ecs_2',['MyNpgsqlBase.cs',['../_my_npgsql_base_8cs.html',1,'']]]
+  ['mynpgsql_2edelete_2ecs_1',['MyNpgsql.Delete.cs',['../_my_npgsql_8_delete_8cs.html',1,'']]],
+  ['mynpgsql_2efetch_2ecs_2',['MyNpgsql.Fetch.cs',['../_my_npgsql_8_fetch_8cs.html',1,'']]],
+  ['mynpgsql_2einsert_2ecs_3',['MyNpgsql.Insert.cs',['../_my_npgsql_8_insert_8cs.html',1,'']]],
+  ['mynpgsql_2eupdate_2ecs_4',['MyNpgsql.Update.cs',['../_my_npgsql_8_update_8cs.html',1,'']]],
+  ['mynpgsqlasync_2ecs_5',['MyNpgsqlAsync.cs',['../_my_npgsql_async_8cs.html',1,'']]],
+  ['mynpgsqlasync_2edelete_2ecs_6',['MyNpgsqlAsync.Delete.cs',['../_my_npgsql_async_8_delete_8cs.html',1,'']]],
+  ['mynpgsqlasync_2efetch_2ecs_7',['MyNpgsqlAsync.Fetch.cs',['../_my_npgsql_async_8_fetch_8cs.html',1,'']]],
+  ['mynpgsqlasync_2einsert_2ecs_8',['MyNpgsqlAsync.Insert.cs',['../_my_npgsql_async_8_insert_8cs.html',1,'']]],
+  ['mynpgsqlasync_2eupdate_2ecs_9',['MyNpgsqlAsync.Update.cs',['../_my_npgsql_async_8_update_8cs.html',1,'']]],
+  ['mynpgsqlbase_2ecs_10',['MyNpgsqlBase.cs',['../_my_npgsql_base_8cs.html',1,'']]]
 ];
--- NpgsqlWrapper/doxygen/html/search/properties_4.js
+++ NpgsqlWrapper/doxygen/html/search/properties_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['lastinsertedid_0',['LastInsertedID',['../class_npgsql_wrapper_1_1_my_npgsql_base.html#af396807711d47807b35b73f35a419514',1,'NpgsqlWrapper::MyNpgsqlBase']]]
+  ['lastinsertedid_0',['LastInsertedID',['../class_npgsql_wrapper_1_1_my_npgsql_base.html#aa8cba64d090188aae079a2289f3dabf2',1,'NpgsqlWrapper::MyNpgsqlBase']]]
 ];
